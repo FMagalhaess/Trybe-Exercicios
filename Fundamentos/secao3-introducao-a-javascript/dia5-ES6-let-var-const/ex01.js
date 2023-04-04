779bab5cfc60948e25d844552aaa63421f7a6f37
¨Filipe <7filipe093@gmail.com>
2023-04-04 19:25:20 -0300

    dia5

---
+++ Fundamentos/secao3-introducao-a-javascript/dia5-ES6-let-var-const/ex01.js
@@ -0,0 +1,6 @@
+const favoriteLanguage = 'Javascript'
+console.log(favoriteLanguage); // Erro
+
+let favoriteTechnology = 'Machine learning';
+favoriteTechnology = 'Facial recognition';
+console.log(favoriteTechnology); // Facial recognition
